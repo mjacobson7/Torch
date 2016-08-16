cc58bd3995789f65a825da67fd17b08d4947149d
Max Jacobson <mjacobson7@gmail.com>
2016-08-16 08:58:25 -0600

    changes to express.js

--- server/config/express.js
+++ server/config/express.js
@@ -10,7 +10,7 @@ module.exports = function() {
   app.use(cors());
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true}));
-  app.use(express.static('../public'));
+  app.use(express.static(__dirname + '/public'));
 
   return app;
 
