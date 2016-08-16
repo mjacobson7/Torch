e7a33dbde147c989fb238cd693e13095bcfc35ed
Max Jacobson <mjacobson7@gmail.com>
2016-08-16 08:55:01 -0600

    removed Procfile

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var app = require('./config/express.js')();
 require('./config/database.js')();
 require('./config/passport')(app);
 require('./app/users/userRoutes.js')(app);
-var port = 3000;
+var port = process.env.PORT || 3000;
 
 app.listen(port, function() {
   console.log('The magic happens on port ' + port);
