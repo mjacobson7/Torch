af02c466b636ba0cfa1bcc50bcd0b2f8de299c27
Max Jacobson <mjacobson7@gmail.com>
2016-08-16 18:16:36 -0600

    update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-var app = require('./config/express.js')();
-require('./config/database.js')();
-require('./config/passport')(app);
-require('./app/users/userRoutes.js')(app);
+var app = require('./server/config/express.js')();
+require('./server/config/database.js')();
+require('./server/config/passport')(app);
+require('./server/app/users/userRoutes.js')(app);
 var port = process.env.PORT || 3000;
 
 app.listen(port, function() {
