9c88bcc930338ad10289fb1462fb215e52c0b38c
Max Jacobson <mjacobson7@gmail.com>
2016-08-19 14:41:33 -0600

    update static file

--- server/config/express.js
+++ server/config/express.js
@@ -7,10 +7,11 @@ module.exports = function() {
 
   var app = express();
 
+  app.use(express.static(__dirname + '/../../public'));
   app.use(cors());
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true}));
-  app.use(express.static(__dirname + '/public'));
+
 
   return app;
 
