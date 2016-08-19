e199e1859c754be5b48a9f4592d36d49a3d6aefc
Max Jacobson <mjacobson7@gmail.com>
2016-08-19 15:53:00 -0600

    updated customer table

--- public/features/customers/customersCtrl.js
+++ public/features/customers/customersCtrl.js
@@ -9,13 +9,13 @@ angular.module('myApp')
 
 
         $scope.customers = [
-          { name: "Toyota", status: "Current Customer (active)", prod: "http://torchlms.com", uat: "http://.torchlms.com"},
-          { name: "Hyundai of Canada", status: "Current Customer (active)", prod: "https://torchlms.com/login/default-view?forceManual=true", uat: "https://torchlms.com/login/default-view?forceManual=true"},
-          { name: "Devereux", status: "Current Customer (active)", prod: "https://torchlms.com/login/defaultView", uat: "https://torchlms.com/login/defaultView"},
-          { name: "Google", status: "Current Customer (active)", prod: "https://google.torchlms.com/login/defaultView", uat: "https://google-uat.torchlms.com/login/defaultView"},
-          { name: "Apple", status: "Current Customer (active)", prod: "https://apple.torchlms.com/login/defaultView", uat: "https://apple-uat.torchlms.com/login/defaultView"},
-          { name: "Facebook", status: "Current Customer (active)", prod: "https://facebook.torchlms.com/login/defaultView", uat: "https://facebook-uat.torchlms.com/login/defaultView"},
-          { name: "Dev Mountain", status: "Current Customer (active)", prod: "https://devmountain.torchlms.com/login/defaultView", uat: "https://devmountain-uat.torchlms.com/login/defaultView"}
+          { name: "Toyota", prod: "http://torchlms.com", uat: "http://.torchlms.com"},
+          { name: "Hyundai of Canada", prod: "https://torchlms.com/login/default-view?forceManual=true", uat: "https://torchlms.com/login/default-view?forceManual=true"},
+          { name: "Devereux", prod: "https://torchlms.com/login/defaultView", uat: "https://torchlms.com/login/defaultView"},
+          { name: "Google", prod: "https://google.torchlms.com/login/defaultView", uat: "https://google-uat.torchlms.com/login/defaultView"},
+          { name: "Apple", prod: "https://apple.torchlms.com/login/defaultView", uat: "https://apple-uat.torchlms.com/login/defaultView"},
+          { name: "Facebook", prod: "https://facebook.torchlms.com/login/defaultView", uat: "https://facebook-uat.torchlms.com/login/defaultView"},
+          { name: "Dev Mountain", prod: "https://devmountain.torchlms.com/login/defaultView", uat: "https://devmountain-uat.torchlms.com/login/defaultView"}
         ]
 
   }); //end
