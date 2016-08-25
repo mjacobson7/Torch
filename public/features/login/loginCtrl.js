d0d9abe94681a95df01c79e092ed5795a33db38c
Max Jacobson <mjacobson7@gmail.com>
2016-08-25 12:15:13 -0600

    clean up

--- public/features/login/loginCtrl.js
+++ public/features/login/loginCtrl.js
@@ -1,16 +1,6 @@
 angular.module('myApp')
   .controller('loginCtrl', function($rootScope, $scope, mainService, $state) {
-
-    $scope.validateIfLoggedIn = function() {
-      mainService.validateIfLoggedIn().then(function(response) {
-        if(!response.data.passport.user) {
-          $state.go('login');
-        }
-      })
-    };
-
-    $scope.validateIfLoggedIn();
-
+    
     $scope.login = function(email, password) {
       var user = {
         email: email,
