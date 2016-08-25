9d1900e2ce41e16bc06fa86b12f2a58b25126126
Max Jacobson <mjacobson7@gmail.com>
2016-08-25 13:58:45 -0600

    config routes

--- public/js/app.js
+++ public/js/app.js
@@ -7,15 +7,15 @@ angular.module('myApp', ['ui.router'])
       url: '/login',
       templateUrl: '../features/login/login.html',
       controller: 'loginCtrl',
-      resolve: {
-        user: function($state, mainService) {
-          mainService.validateIfLoggedIn().then(function(response) {
-            if(response.data.passport.user) {
-              $state.go('customers');
-            }
-          })
-        }
-      }
+      // resolve: {
+      //   user: function($state, mainService) {
+      //     mainService.validateIfLoggedIn().then(function(response) {
+      //       if(response.data.passport.user) {
+      //         $state.go('customers');
+      //       }
+      //     })
+      //   }
+      // }
     })
 
     .state('userProfile', {
@@ -63,6 +63,8 @@ angular.module('myApp', ['ui.router'])
       }
     })
 
-    $urlRouterProvider.otherwise('/login');
+
+
+    $urlRouterProvider.otherwise('/customers');
 
 }); //end
