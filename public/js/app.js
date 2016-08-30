f17758ccd70d309c64774900192dae2947dfbd3f
Max Jacobson <mjacobson7@gmail.com>
2016-08-30 14:57:41 -0600

    update

--- public/js/app.js
+++ public/js/app.js
@@ -75,6 +75,7 @@ angular.module('myApp', ['ui.router'])
       resolve: {
         user: function($rootScope, $state, mainService) {
           mainService.validateIfLoggedIn().then(function(response) {
+            console.log("THIS => " + response);
             if(!response.data._id) {
               $state.go('login');
             }
