61a99ffabc9329425c536d4df77c7a71f6bd0c81
Max Jacobson <mjacobson7@gmail.com>
2016-09-09 14:58:24 -0600

    update sign-up

--- server/config/passport.js
+++ server/config/passport.js
@@ -69,7 +69,7 @@ module.exports = function(app) {
                newUser.save(function(err) {
                    if (err)
                        throw err;
-                   return done(null, newUser);
+                   return done(null);
                });
            }
 
